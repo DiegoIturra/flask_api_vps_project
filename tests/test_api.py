5f011c07f7fe6921b55e7d82932f4abae9bf4230
DiegoIturra <diego269707@gmail.com>
2026-09-08 18:07:26 -0300

    test: it added pytest dependency and create the first test to test health endpoint

---
+++ tests/test_api.py
@@ -0,0 +1,18 @@
+import pytest
+
+from api import app
+
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+
+    with app.test_client() as client:
+        yield client
+
+
+def test_health(client):
+    response = client.get("/health")
+
+    assert response.status_code == 200
+    assert response.get_json() == {"status": "ok"}
